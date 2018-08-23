73c33a8fee653a70dee4b934b5433f365b3aa291
Jimmy Guo <jimmyzidanguo@hotmail.com>
2018-08-23 16:34:35 -0400

    asd

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 8888:8888 --name=nodeultimate nodeultimate npm run prod -- --host=0.0.0.0
+docker run -d -it -p 8888:8888 --name=dianhua_microservice dianhua_microservice npm run prod -- --host=0.0.0.0
